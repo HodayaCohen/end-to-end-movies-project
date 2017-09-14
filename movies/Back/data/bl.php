f16cdecc9a6c5510880ab624738f382c7db76419
chani <click0548450396@gmail.com>
2017-09-15 01:30:46 +0300

    121212

--- movies/Back/data/bl.php
+++ movies/Back/data/bl.php
@@ -2,20 +2,25 @@
 require_once ('DAL.php');
 
 class BL {
+private $DB;
+
+    function __construct() {
+        $this->DB = new DAL();
+        
+
+    }
 
     
 // selects all from a table in a DB and returns it as array
   function SelectAllFromTable($table_name, $classname) {
-        $DB = new DAL();
-        $res = $DB->GetAllTable("SELECT * FROM `".$table_name."`", $classname);
+        $res = $this->DB->GetAllTable("SELECT * FROM `".$table_name."`", $classname);
         return $res;
 
     }
 
 // checks if a id exists on a id row in a DB and returns true or false
  function Check_if_id_exists($table_name, $id) {
-        $DB = new DAL();
-        $res =  $DB->CheckId("SELECT id FROM ".$table_name." WHERE id='$id'");
+        $res =  $this->DB->CheckId("SELECT id FROM ".$table_name." WHERE id='$id'");
         $istrue = ($res > 0 ?  true : false);
         return $istrue;
     }
@@ -23,29 +28,35 @@ class BL {
 
  // updates data in a table 
  function update_table($table_name, $id, $updateValues) {
-        $DB = new DAL();
-        $DB = $DB->updateSQL("UPDATE ".$table_name." SET ".$updateValues." WHERE id='$id'");
-        return $DB;
+        $update = $this->DB->updateSQL("UPDATE ".$table_name." SET ".$updateValues." WHERE id='$id'");
+        return $update;
 
 }
 
 
  function create_new_row($table_name, $column, $values, $exicute) {
-        $DB = new DAL();
         $query = "INSERT INTO ".$table_name."(".$column.") VALUES (".$values.")";
-        $Create = $DB->insertSQL($query, $exicute);
+        $Create = $this->DB->insertSQL($query, $exicute);
         return $Create;
 
 
 }
 
  function DeleteRow($table_name, $id) {
-        $DB = new DAL();
-        $DB = $DB->getDB();
+    $getdb = $this->DB->getDB();
+    
 
-        $delete =  $DB->prepare("DELETE FROM ".$table_name." WHERE id =". $id);
+        $delete =  $getdb->prepare("DELETE FROM ".$table_name." WHERE id =". $id);
         $delete->execute();
         return $delete;
+
+}
+
+
+function innerJoin($table_name, $column, $join1, $join2) {
+    $innerJion = $this->DB->prepare("SELECT * FROM ".$table_name." INNER JOIN  ".$column." ON  ".$join1." = ".$join2);
+    $innerJion->execute();
+    return $innerJion;
 
 }
 
--- movies/Front/scripts/mainScript.js
+++ movies/Front/scripts/mainScript.js
@@ -40,6 +40,7 @@ function wasDone(response_text, calltype) {
 
 }
 
+
 function insertlist(response_text) {
     $("#select_id").html(response_text);
 }
@@ -55,34 +56,4 @@ function idtest(response_text) {
         $("#id_error").html("");
         $("#hide").removeClass("hide");
     }
-}
-
-//  Create a table with all data
-function creaTable(response_text) {
-    document.getElementById('result').innerHTML = "";
-    var array = response_text;
-    var tableBody = document.getElementById('result');
-    var tr = document.createElement('TR');
-    tr.setAttribute("id", 'th');
-    tableBody.appendChild(tr);
-    var keys = Object.keys(array[0]);
-    for (i = 0; i < keys.length; i++) {
-        var th = document.createElement('TH');
-        th.appendChild(document.createTextNode(keys[i]));
-        document.getElementById('th').appendChild(th);
-    }
-
-
-    for (i = 0; i < array.length; i++) {
-        var tr = document.createElement('TR');
-        tr.setAttribute("id", i);
-        tableBody.appendChild(tr);
-
-        for (var prop in array[i]) {
-            var td = document.createElement('TD');
-            td.appendChild(document.createTextNode(array[i][prop]));
-            document.getElementById(i).appendChild(td);
-        }
-    }
-
 }
